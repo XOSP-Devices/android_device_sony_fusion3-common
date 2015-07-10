86c137549915bae35f88c327b43a0953023c49ae
Steven Veigas <sveiga@codeaurora.org>
2015-07-10 11:04:32 +0000

    fusion3: board: disabled serial logs for faster boot
    Serial logs at kernel are disabled. Change 'BOARD_ENABLE_SERIAL_CONSOLE'
    to 'true' to enable serial console
    
    Change-Id: Ie9fb7d8f660f5896b12ff2db520e1e84b5939569
    Signed-off-by: AlexM0rtis <alex.mihalenok@gmail.com>

--- board/kernel.mk
+++ board/kernel.mk
@@ -1,7 +1,8 @@
 # Kernel informations
 BOARD_KERNEL_BASE     := 0x80200000
 BOARD_KERNEL_PAGESIZE := 2048
-BOARD_KERNEL_CMDLINE  := androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3
+BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3
+BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000
 KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-eabi-4.8/bin
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := arm-eabi-
 
